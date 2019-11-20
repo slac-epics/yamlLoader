31dc8d077c4823b3e6abdf94266b80c69aed4dfe
Kukhee Kim <khkim@slac.stanford.edu>
2019-11-20 13:08:05 -0800

    support multiple ATCA blades in a single ioc (multiple roots) with backward-compatible mode

--- yamlLoaderApp/src/yamlLoader.cpp
+++ yamlLoaderApp/src/yamlLoader.cpp
@@ -6,6 +6,9 @@
 #include <cpsw_api_user.h>
 #include <yamlLoader.h>
 
+#include <cantProceed.h>
+#include <epicsString.h>
+#include <ellLib.h>
 #include <iocsh.h>
 #include <drvSup.h>
 #include <epicsExport.h>
@@ -28,23 +31,89 @@ private:
 
 extern "C" {
 // need to think more about the ugly global one
-static Path theRoot;
 
+static ELLLIST *pRootList = NULL;
+
+typedef struct {
+    ELLNODE      node; 
+    Path         root;
+    char         *description;
+    char         *name;
+} rootList_t;
+
+static void init_rootList(void)
+{
+
+    if(!pRootList) {
+        pRootList = (ELLLIST*) mallocMustSucceed(sizeof(ELLLIST), "linked list for root list in yamlLoader driver");
+        ellInit(pRootList);
+    }
+}
+
+int cpswIsThereRoot(void)
+{
+    init_rootList();
+    return ellCount(pRootList);
+}
 
 Path cpswGetRoot(void)
 {
-    return theRoot;
+    init_rootList();
+    rootList_t *p = NULL;
+    if(ellCount(pRootList)) p = (rootList_t *) ellLast(pRootList);
+   
+    
+    return p->root;
+}
+
+Path cpswGetNamedRoot(const char *name)
+{
+    int rootList();
+
+    rootList_t *p = (rootList_t *) ellFirst(pRootList);
+    while(p) {
+        if(!strcmp(p->name, name)) break;
+        p = (rootList_t *) ellNext(&p->node);
+    }
+
+    return p->root;
 }
 
 void cpswPutRoot(Path root)
 {
-    theRoot = root;
+    init_rootList();
+
+    char name[80];
+    sprintf(name, "root_%d", ellCount(pRootList));
+    rootList_t *p = (rootList_t*) mallocMustSucceed(sizeof(rootList_t), "Root List in yamlLoader driver");
+    p->name = epicsStrDup(name);
+    p->root = root;
+
+    ellAdd(pRootList, &p->node);
+    
 }
+
+
+void cpswPutNamedRoot(Path root, const char *name)
+{
+    init_rootList();
+
+    rootList_t *p = (rootList_t *) mallocMustSucceed(sizeof(rootList_t), "Root List in yamlLoader driver");
+    p->name = epicsStrDup(name);
+    p->root = root;
+
+    ellAdd(pRootList, &p->node);
+
+}
+
+
 
 
 
 int cpswLoadYamlFile(const char *yaml_file, const char *root, const char *yaml_dir, const char *ip_addr)
 {
+    Path theRoot;
+
     if(!yaml_file) {
         fprintf(stderr, "Missing YAML file name\n");
         return -1;
@@ -56,11 +125,14 @@ int cpswLoadYamlFile(const char *yaml_file, const char *root, const char *yaml_d
               IYamlSetIP setIP(ip_addr);
               theRoot = IPath::loadYamlFile(yaml_file, root, yaml_dir, &setIP);
         }
+
+        cpswPutRoot(theRoot);
         
     } catch (CPSWError &e) {
         fprintf(stderr, "Unable to load YAML file: %s\n", e.getInfo().c_str());
         return -1;
     }
+
     
     return 0;
 }
@@ -69,7 +141,7 @@ int cpswLoadConfigFile(const char *yaml_file, const char *prefix, const char *ya
 {
     Path pre;
     
-    if(!theRoot) {
+    if(!cpswIsThereRoot()) {
         fprintf(stderr, "No hierarchy found; use 'cpswLoadYamlFile(with HIERARCHY yaml, not config yaml)\n");
         return -1;
     }
@@ -80,6 +152,7 @@ int cpswLoadConfigFile(const char *yaml_file, const char *prefix, const char *ya
     }
     
     try {
+        Path theRoot = cpswGetRoot();
         pre = prefix ? theRoot->findByName(prefix) : theRoot;
     } catch (CPSWError &e) {
         fprintf(stderr, "Unable to load CONFIG file: %s\n", e.getInfo().c_str());
@@ -97,7 +170,7 @@ int cpswDumpConfigFile(const char *yaml_file, const char *prefix, const char *ya
 {
     Path pre;
     
-    if(!theRoot) {
+    if(!cpswIsThereRoot()) {
         fprintf(stderr, "No hierarchy found; use 'clswLoadYamlFile(with HIERARCHY yaml, cont config yaml)\n");
         return -1;
     }
@@ -108,6 +181,7 @@ int cpswDumpConfigFile(const char *yaml_file, const char *prefix, const char *ya
     }
     
     try {
+        Path theRoot = cpswGetRoot();
         pre = prefix ? theRoot->findByName(prefix) : theRoot;
     } catch (CPSWError &e) {
         fprintf(stderr, "Unable to dump Config file: %s\n", e.getInfo().c_str());
@@ -233,6 +307,15 @@ epicsExportAddress(drvet, yamlLoaderDriver);
 
 static int yamlLoaderReport(int interest)
 {
+    printf("found %d root(s)\n", cpswIsThereRoot());
+    if(cpswIsThereRoot()) {
+        rootList_t *p = (rootList_t *) ellFirst(pRootList);
+        while(p) {
+            printf("\t%s\n", p->name);
+            p = (rootList_t *) ellNext(&p->node);
+        }
+    }
+
     return 0;
 }
 
--- yamlLoaderApp/src/yamlLoader.h
+++ yamlLoaderApp/src/yamlLoader.h
@@ -7,8 +7,11 @@
 extern "C" { 
 #endif /* __cplusplus */
 
+int cpswIsThereRoot(void);
 Path cpswGetRoot(void);
 void cpswPutRoot(Path root);
+Path cpswGetNamedRoot(const char *name);
+void cpswPutNamedRoot(Path root, const char *name);
 int cpswLoadYamlFile(const char *yaml_file, const char *root, const char *yaml_dir, const char *ip_addr);
 int cpswLoadConfigFile(const char *yaml_file, const char *prefix, const char *yaml_dir);
 int cpswDumpConfigFile(const char *yaml_file, const char *prefix, const char *yaml_dir);
