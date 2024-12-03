d6ddc79440adce770cdd843ffdfa06e9bbda7ab3
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-12-03 15:52:12 -0800

    Apply SLAC license

--- yamlLoaderApp/src/yamlLoader.cpp
+++ yamlLoaderApp/src/yamlLoader.cpp
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////////////////
+// This file is part of 'yamlLoader'.
+// It is subject to the license terms in the LICENSE.txt file found in the 
+// top-level directory of this distribution and at: 
+//    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+// No part of 'yamlLoader', including this file, 
+// may be copied, modified, propagated, or distributed except according to 
+// the terms contained in the LICENSE.txt file.
+//////////////////////////////////////////////////////////////////////////////
 /* SLAC common platform  - yaml loader epics driver */
 
 #include <fstream>
